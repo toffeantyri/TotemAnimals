c6d464da80b31d5a3263d954bb27619a3ae06746
toffeantyri <tofy3434@yandex.ru>
2021-12-20 17:53:30 +0600

    0.15 Добавлен data class - шаблон для передачи картинки и названия Добавлен class адаптер для передачи в recyclerView с class ом принимающим view элементы из animal_shablon_xml (карточка которая является шаблоном для заполнения картинкой и названием) Не доделана передаяа в адаптер

---
+++ app/src/main/java/com/example/totemanimals/animals_adapt_list.kt
@@ -0,0 +1,12 @@
+package com.example.totemanimals
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class AnimalsAdaptList: RecyclerView.Adapter<AnimalsAdaptList.AnimalHolder> {
+    class AnimalHolder(animal_item: View) : RecyclerView.ViewHolder(animal_item){
+        fun bind(shablonAnimal: shablon_animal) {
+            //val im =
+        }
+    }
+}
---
+++ app/src/main/java/com/example/totemanimals/shablon_animal.kt
@@ -0,0 +1,5 @@
+package com.example.totemanimals
+
+data class shablon_animal (val im_animal:Int, val title_animal:String) {
+
+}
