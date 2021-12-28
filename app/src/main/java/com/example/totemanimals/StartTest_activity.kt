9e574267e736e177bf5558a0f0e2b10dea8dbe99
toffeantyri <tofy3434@yandex.ru>
2021-12-28 15:46:11 +0600

    0.22 fix инфо фрагмент по наполнению стал

---
+++ app/src/main/java/com/example/totemanimals/StartTest_activity.kt
@@ -0,0 +1,12 @@
+package com.example.totemanimals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartTest_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_test_activity)
+    }
+}
