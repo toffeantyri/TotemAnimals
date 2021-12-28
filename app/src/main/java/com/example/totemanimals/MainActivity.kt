9e1907cfc98664782ac4794e64cc217677a73952
toffeantyri <tofy3434@yandex.ru>
2021-12-28 16:39:06 +0600

    0.23 fix инфо фрагмент по наполнению стал? исправлено повторно?
    Добавлено активити для прохождения теста
    Добавлен фрагмент который открыт при открытии приложения (Окно результатов теста)
    реализована передача интента для активити прохождения теста

--- app/src/main/java/com/example/totemanimals/MainActivity.kt
+++ app/src/main/java/com/example/totemanimals/MainActivity.kt
@@ -28,25 +28,17 @@ import kotlin.contracts.ContractBuilder
 class MainActivity : AppCompatActivity() {
     lateinit var mAdView: AdView
     private val adapter1 = AnimalsAdaptList()
-//    val imIdList = listOf(R.drawable.smallbelka,R.drawable.smallbober,R.drawable.smallezh,
-//        R.drawable.smallfilin,R.drawable.smallkon,R.drawable.smalllebed,R.drawable.smalllisa,
-//        R.drawable.smallmedved,R.drawable.smallolen,R.drawable.smallorel,R.drawable.smallpetuh,
-//        R.drawable.smallslon,R.drawable.smalltur,R.drawable.smalluzh,R.drawable.smallvolk,)
-//    val big_imIdList = listOf(R.drawable.smallbelka,R.drawable.smallbober,R.drawable.smallezh,
-//        R.drawable.smallfilin,R.drawable.smallkon,R.drawable.smalllebed,R.drawable.smalllisa,
-//        R.drawable.smallmedved,R.drawable.smallolen,R.drawable.smallorel,R.drawable.smallpetuh,
-//        R.drawable.smallslon,R.drawable.smalltur,R.drawable.smalluzh,R.drawable.smallvolk,)
-//
-//    val nameIdList = listOf("Белка","Бобёр","Ёж","Филин","Конь","Лебедь","Лисица","Медведь","Олень","Орёл","Петух","Слон","Тур","Уж","Волк",)
-//    val descriptIdList = listOf("one", "two", "three","four", "five","six","seven", "eigth", "nine","ten","eleven","twelf", "1three", "1four","1five",)
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
         addAllAnimalOnRV()
+        rcView_AnList.visibility = View.GONE
+        my_info_frame.visibility = View.GONE
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.my_testResult_frame, fragment_testResult()).commit()
+        my_testResult_frame.visibility = View.VISIBLE
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // Отключение ночной темы для этого активити
 
@@ -56,63 +48,82 @@ class MainActivity : AppCompatActivity() {
         mAdView.loadAd(adRequest)
 
         nav_bottom_menu.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.home_menu_id -> {
-                    rcView_AnList.visibility=View.GONE
-                    my_info_frame.visibility=View.GONE
+                    rcView_AnList.visibility = View.GONE
+                    my_info_frame.visibility = View.GONE
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.my_testResult_frame, fragment_testResult()).commit()
+                    my_testResult_frame.visibility = View.VISIBLE
+
+//                    val intent = Intent(this, StartTest_activity::class.java)
+//                    intent.putExtra("start_test", "start")
+//                    startActivityForResult(intent, 2)
                 }
                 R.id.search_menu_id -> {
-                    rcView_AnList.visibility=View.VISIBLE
-                    my_info_frame.visibility=View.GONE
-                                    }
-                R.id.info_menu_id -> {
-
-                    rcView_AnList.visibility=View.GONE
-                    Log.d("MyLog","count:${list_resours.imIdList.count()} Количество итемов: ${rcView_AnList.layoutManager?.itemCount}   ")
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.my_info_frame,fragment_info()).commit()
-                    my_info_frame.visibility=View.VISIBLE
+                    rcView_AnList.visibility = View.VISIBLE
+                    my_info_frame.visibility = View.GONE
+                    my_testResult_frame.visibility = View.GONE
                 }
-                R.id.exit_menu_id -> { finish() }
+                R.id.info_menu_id -> {
+                    my_testResult_frame.visibility = View.GONE
+                    rcView_AnList.visibility = View.GONE
+                    Log.d("MyLog","count:${list_resours.imIdList.count()} Количество итемов: ${rcView_AnList.layoutManager?.itemCount}   ")
+
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.my_info_frame, fragment_info()).commit()
+                    my_info_frame.visibility = View.VISIBLE
+                }
+                R.id.exit_menu_id -> {
+                    finish()
+                }
             }
             true
         }
 
-
-
     }
 
-
-    fun addAllAnimalOnRV () {
-        if(list_resours.imIdList.count()!=rcView_AnList.layoutManager?.itemCount){
-            var index = 0
-            var a : Int
-            var a1: Int
-            var b: String
-            var c: String
-            var animalAdd : Animal
-            for (i in list_resours.imIdList) {
-                a = list_resours.imIdList[index]
-                a1 = list_resours.big_imIdList[index]
-                b = list_resours.nameIdList[index]
-                c = list_resours.descriptIdList[index]
-                animalAdd = Animal(a, a1, b, c)
-                adapter1.addAnimal(animalAdd)
-                index++
+        fun addAllAnimalOnRV() {
+            if (list_resours.imIdList.count() != rcView_AnList.layoutManager?.itemCount) {
+                var index = 0
+                var a: Int
+                var a1: Int
+                var b: String
+                var c: String
+                var animalAdd: Animal
+                for (i in list_resours.imIdList) {
+                    a = list_resours.imIdList[index]
+                    a1 = list_resours.big_imIdList[index]
+                    b = list_resours.nameIdList[index]
+                    c = list_resours.descriptIdList[index]
+                    animalAdd = Animal(a, a1, b, c)
+                    adapter1.addAnimal(animalAdd)
+                    index++
+                }
             }
         }
-        }
 
 
-    private fun init() {
-        rcView_AnList.layoutManager = GridLayoutManager(this@MainActivity, 3)
-        rcView_AnList.adapter = adapter1
-        adapter1.onItemClick = {Animal ->
-            val intent = Intent(this, Animal_descpt_view::class.java)
-            intent.putExtra("description_search",Animal)
-            startActivity(intent)
+        private fun init() {
+            rcView_AnList.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            rcView_AnList.adapter = adapter1
+            adapter1.onItemClick = { Animal ->
+                val intent = Intent(this, Animal_descpt_view::class.java)
+                intent.putExtra("description_search", Animal)
+                startActivity(intent)
 
             }
+        }
+
+        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            super.onActivityResult(requestCode, resultCode, data)
+            if (requestCode == 2 && resultCode == Activity.RESULT_CANCELED && data != null) {
+                val myIntent1 = data.getStringExtra("123")
+                Toast.makeText(this@MainActivity, "вернулся с резульаттом: $myIntent1", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
-}
+
 
--- app/src/main/java/com/example/totemanimals/StartTest_activity.kt
+++ app/src/main/java/com/example/totemanimals/StartTest_activity.kt
@@ -1,12 +1,21 @@
 package com.example.totemanimals
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start_test_activity.*
 
 class StartTest_activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_test_activity)
+        val butText = intent.getStringExtra("start_test")!!
+
+        btn_close_test.setOnClickListener{
+            intent.putExtra("123", "123")
+            setResult(Activity.RESULT_CANCELED,intent)
+            finish()
+        }
     }
 }
