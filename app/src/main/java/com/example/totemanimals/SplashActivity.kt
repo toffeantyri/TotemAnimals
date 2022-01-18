eb746ec78daeb4f9b653059d73bcb9184cfe5181
toffeantyri <tofy3434@yandex.ru>
2022-01-18 14:23:02 +0600

    0.397 Сплаш скрин, фик баг в стили добавлен фон иконка анимированна

--- app/src/main/java/com/example/totemanimals/SplashActivity.kt
+++ app/src/main/java/com/example/totemanimals/SplashActivity.kt
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val intent = Intent(this,MainActivity::class.java)
-        startActivity(intent)
-        finish()
+
+
+        im_logo.alpha = 0f
+        im_logo.animate().setDuration(1000).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
-
-
 }
