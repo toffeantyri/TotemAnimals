50c00eaf5c83d3c3d60f8b569675d57e76cc2c93
toffeantyri <tofy3434@yandex.ru>
2022-01-25 17:24:48 +0600

    0.8 в активити desc view (описание животного) добавлена кнопка share
    добавлена функция imageToUri превращабщая image id в Uri ссылку.
    По кнопке Share отправка интента - потребуется ссылка на приложение в гугл плей

--- app/src/main/java/com/example/totemanimals/Animal_descpt_view.kt
+++ app/src/main/java/com/example/totemanimals/Animal_descpt_view.kt
@@ -1,8 +1,13 @@
 package com.example.totemanimals
 
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
 import android.util.Log
+import com.google.android.material.internal.ParcelableSparseArray
 import kotlinx.android.synthetic.main.activity_animal_descpt_view.*
 import java.io.Serializable
 
@@ -19,6 +24,11 @@ class Animal_descpt_view : BaseActivity_ApComAct() {
         finish()
         }
 
+
+        btn_share_desc_animal.setOnClickListener {
+            shareDescAnimal()
+        }
+
     }
 
     fun bindingViewLyout () {
@@ -26,4 +36,32 @@ class Animal_descpt_view : BaseActivity_ApComAct() {
         desc_act_tv_title.text = descAnimal_data.title_animal
         desc_act_tv_desc.text = descAnimal_data.disc
     }
+
+    fun shareDescAnimal(){
+        val link = getString(R.string.link_on_my_app_for_share)
+        val pic = imageToUri(descAnimal_data.im_animal)
+        val my_totem = getString(R.string.my_totem_text)
+        val title = descAnimal_data.title_animal + "\n"
+        val descr = descAnimal_data.disc
+
+
+        val intent = Intent()
+        intent.setAction(Intent.ACTION_SEND)
+        intent.putExtra(Intent.EXTRA_STREAM, pic )
+        intent.putExtra(Intent.EXTRA_TEXT, link + "\n" + my_totem +  title + "\n" + descr )
+        intent.setType("image/*")
+        Log.d("MyLog", link +"\n " + pic +"\n " + my_totem + title +"\n " + descr)
+        startActivity(Intent.createChooser(intent, "Рассказать о своём тотемном животном:"))
+
+    }
+
+    //преобразует ресурс Id картинки в Uri
+    fun imageToUri(imId: Int): Uri  {
+        val image_uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + resources.getResourcePackageName(imId)+
+                "/" + resources.getResourceTypeName(imId) +
+                "/" + resources.getResourceEntryName(imId))
+        return image_uri
+    }
 }
+
